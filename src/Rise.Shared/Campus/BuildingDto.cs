f398f42732b113e8a8bd78adf7378deed73a3ef8
Mariana Abukaeva <mariana.abukaeva@student.hogent.be>
2025-10-25 16:04:48 +0200

    GetBuilding endpoint,dto and services
    + mock data aangepast

--- src/Rise.Shared/Campus/BuildingDto.cs
+++ src/Rise.Shared/Campus/BuildingDto.cs
@@ -2,10 +2,17 @@ namespace Rise.Shared.Campus;
 
 public class BuildingDto
 {
-    public required string Id { get; set; }
-    public required string Name { get; set; }
-    public required string Code { get; set; }
-    public int? X { get; set; } // pixel, for image plans
-    public int? Y { get; set; }
-    public required string Type { get; set; }
+    public class Index
+    {
+        public required string Id { get; set; }
+        public required string CampusId { get; set; }
+        public required string Name { get; set; }
+        public required string Address { get; set; }
+        public required string Type { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public int? X { get; set; }
+        public int? Y { get; set; }
+        // public List<ClassroomDto>? ClassRooms { get; set; }
+    }
 }
