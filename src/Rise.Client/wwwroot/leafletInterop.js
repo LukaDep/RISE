05d1054b15bc8642405a511491e77ea5b40baea3
Mariana Abukaeva <mariana.abukaeva@student.hogent.be>
2025-10-25 17:34:14 +0200

    Link from schedule classroom to campusplan
    + link to google maps of building

--- src/Rise.Client/wwwroot/leafletInterop.js
+++ src/Rise.Client/wwwroot/leafletInterop.js
@@ -10,7 +10,7 @@ window.leafletMap = {
             minZoom: -2,
             maxBounds: bounds,
             maxBoundsViscosity: 1.0,
-            zoomControl: false, // Disable default zoom control
+            zoomControl: false,
         })
         this.imageLayer = L.imageOverlay(imageUrl, bounds).addTo(this.map)
         this.map.fitBounds(bounds)
@@ -18,7 +18,6 @@ window.leafletMap = {
         var minZoom = this.map.getBoundsZoom(bounds, false)
         this.map.setMinZoom(minZoom)
 
-        // Add zoom control to bottom right AFTER everything else
         L.control.zoom({ position: 'bottomright' }).addTo(this.map)
     },
     addMarker: function (lat, lng, name) {
@@ -35,12 +34,33 @@ window.leafletMap = {
             attribution: '© OpenStreetMap contributors',
         }).addTo(this.map)
 
-        // Move zoom control for tile map as well
         this.map.zoomControl.setPosition('bottomright')
     },
     addLatLngMarker: function (lat, lng, name) {
         if (this.map) {
             L.marker([lat, lng]).addTo(this.map).bindPopup(name)
+        }
+    },
+    addMarkerWithGoogleLink: function (lat, lng, name) {
+        if (!this.map) return
+        var marker = L.marker([lat, lng]).addTo(this.map)
+        var gmaps =
+            'https://www.google.com/maps/search/?api=1&query=' + lat + ',' + lng
+        var safeName = (name || '').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+        var html =
+            '<div><strong>' +
+            safeName +
+            '</strong><br/><a href="' +
+            gmaps +
+            '" target="_blank" rel="noopener">Open in Google Maps</a></div>'
+        marker.bindPopup(html)
+    },
+    setView: function (lat, lng, zoom) {
+        if (!this.map) return
+        if (typeof zoom === 'number') {
+            this.map.setView([lat, lng], zoom)
+        } else {
+            this.map.setView([lat, lng])
         }
     },
 }
