c723dcc63478b41e1c1c974fe82913ed4f261848
Zepje <seppe.landtsheer@gmail.com>
2025-10-28 17:15:08 +0100

    contact opzoeken / opzoeken via id

--- src/Rise.Services/ServiceCollectionExtensions.cs
+++ src/Rise.Services/ServiceCollectionExtensions.cs
@@ -15,6 +15,8 @@ using Rise.Shared.Resto;
 using Rise.Shared.CampusInfo;
 using Rise.Services.CampusInfo;
 using Rise.Shared.Schedule;
+using Rise.Services.Contact;
+using Rise.Shared.Contact;
 
 namespace Rise.Services;
 
@@ -31,6 +33,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IRestoService, MockRestoService>();
         services.AddScoped<INewsService, NewsService>();
         services.AddScoped<ICampusInfoService, CampusInfoService>();
+        services.AddScoped<IContactService, ContactService>();
         services.AddTransient<DbSeeder>();
 
         // Add other application services here.
---
+++ src/Rise.Shared/Contact/ContactDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Rise.Shared.Contact;
+
+public static class ContactDto
+{
+    public class Index
+    {
+        public required int Id { get; set; }
+        public required string Type { get; set; }
+        public required string Name { get; set; }
+        public string? Email { get; set; }
+        public string? phoneNumber { get; set; }
+        public string? ContactPerson { get; set; }
+        public List<int>? Campusses { get; set; }
+    }
+}
