8c94a8873782d69471632314153a80f3d946cb56
LukaDep <160613429+LukaDep@users.noreply.github.com>
2025-10-29 10:25:03 +0100

    aanpassingen aan menu page,

--- src/Rise.Client/Menu/DayMenu.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-using Rise.Shared.Menu;
-using Rise.Client.Menu;
-
-public class DayMenuBase : ComponentBase
-{
-    [Parameter] public DateTime Date { get; set; }
-    [Parameter] public List<MenuItemDto.Index> MenuItems { get; set; } = new();
-}
--- src/Rise.Client/Menu/MenuPage.razor.cs
+++ src/Rise.Client/Menu/MenuPage.razor.cs
@@ -7,18 +7,26 @@ namespace Rise.Client.Menu;
 public partial class MenuPage : ComponentBase
 {
     [Parameter] public string RestoId { get; set; } = default!;
+
     [Inject] private IMenuService MenuService { get; set; } = default!;
+    [Inject] private NavigationManager NavigationManager { get; set; } = default!;
 
     protected List<MenuDto.Index> menus = new();
     protected bool isLoading = true;
     protected HashSet<DateTime> expandedDays = new();
-
     protected MenuFilter currentFilter = MenuFilter.All;
 
     protected override async Task OnInitializedAsync()
     {
+        await LoadMenusAsync();
+    }
+
+    private async Task LoadMenusAsync()
+    {
         try
         {
+            isLoading = true;
+
             var result = await MenuService.GetIndexAsync(new QueryRequest.SkipTake
             {
                 Skip = 0,
@@ -33,6 +41,11 @@ public partial class MenuPage : ComponentBase
                     .ToList();
             }
         }
+        catch (Exception ex)
+        {
+            // eventueel logging toevoegen
+            Console.Error.WriteLine($"[MenuPage] Error loading menus: {ex.Message}");
+        }
         finally
         {
             isLoading = false;
@@ -41,10 +54,8 @@ public partial class MenuPage : ComponentBase
 
     protected void ToggleDay(DateTime day)
     {
-        if (expandedDays.Contains(day))
+        if (!expandedDays.Add(day))
             expandedDays.Remove(day);
-        else
-            expandedDays.Add(day);
     }
 
     protected void SetFilter(MenuFilter filter)
@@ -52,14 +63,17 @@ public partial class MenuPage : ComponentBase
         currentFilter = filter;
     }
 
-    protected bool ShouldShowItem(MenuItemDto.Index item)
-    {
-        return currentFilter switch
+    protected bool ShouldShowItem(MenuItemDto.Index item) =>
+        currentFilter switch
         {
             MenuFilter.Veggie => item.IsVeggie,
             MenuFilter.Vegan => item.IsVegan,
             _ => true
         };
+
+    protected void GoBack()
+    {
+        NavigationManager.NavigateTo("/resto");
     }
 
     protected enum MenuFilter
