c89739d9202b37364402cc7a4c4e43bab46ee849
Mariana Abukaeva <mariana.abukaeva@student.hogent.be>
2025-10-29 18:43:45 +0100

    Grades scherm + aanpassingen backend
    + filters

--- src/Rise.Client/Grades/GradesClientService.cs
+++ src/Rise.Client/Grades/GradesClientService.cs
@@ -2,19 +2,20 @@ namespace Rise.Client.Grades;
 
 using Rise.Shared.Grades;
 using System.Net.Http.Json;
+using System.Text.Json;
 using Rise.Shared.Common;
 
 
 public class GradesClientService(HttpClient httpClient) : IGradesService
 {
-    public async Task<Result<GradesResponse.CourseList>> GetCoursesAsync(QueryRequest.SkipTake request, CancellationToken ctx = default)
+    public async Task<Result<GradesResponse.Index>> GetIndexAsync(QueryRequest.SkipTake request, CancellationToken ctx = default)
     {
-        var result = await httpClient.GetFromJsonAsync<Result<GradesResponse.CourseList>>($"/api/grades", cancellationToken: ctx);
+        var result = await httpClient.GetFromJsonAsync<Result<GradesResponse.Index>>($"/api/grades?searchterm={request.SearchTerm}&skip={request.Skip}&take={request.Take}", cancellationToken: ctx);
         return result!;
     }
-    public async Task<Result<GradesResponse.CourseById>> GetCourseByIdAsync(string courseId, CancellationToken ctx = default)
+    public async Task<Result<GradesResponse.GradeById>> GetGradeByIdAsync(string gradeId, CancellationToken ctx = default)
     {
-        var result = await httpClient.GetFromJsonAsync<Result<GradesResponse.CourseById>>($"/api/grades/{courseId}", cancellationToken: ctx);
+        var result = await httpClient.GetFromJsonAsync<Result<GradesResponse.GradeById>>($"/api/grades/{gradeId}", cancellationToken: ctx);
         return result!;
     }
 }
--- src/Rise.Shared/Grades/GradesDto.cs
+++ src/Rise.Shared/Grades/GradesDto.cs
@@ -1,16 +1,19 @@
+namespace Rise.Shared.Grades;
+
 public static class GradesDto
 {
-    public class Course
-    {
-        public required string CourseId { get; set; }
-        public required string CourseName { get; set; }
-        public required int Year { get; set; }
-        public required int Semester { get; set; }
-        public double? FinalScore { get; set; }
-        public required List<Grade> Grades { get; set; }
-    }
+    // public class Course
+    // {
+    //     public required string CourseId { get; set; }
+    //     public required string CourseName { get; set; }
+    //     public required int Year { get; set; }
+    //     public required int Semester { get; set; }
+    //     public double? FinalScore { get; set; }
+    //     public required List<Grade> Grades { get; set; }
+    // }
     public class Grade
     {
+        // grade
         public required string Id { get; set; }
         public required string Name { get; set; }
         public required string ActivityType { get; set; }
@@ -18,7 +21,12 @@ public static class GradesDto
         public double? Score { get; set; }
         public string? Feedback { get; set; }
         public DateTime? SubmissionDate { get; set; }
-        public DateTime DueDate { get; set; }
+        public DateTime Date { get; set; }
 
+        // course
+        public string? CourseId { get; set; }
+        public string? CourseName { get; set; }
+        public string? Year { get; set; }
+        public int? Semester { get; set; }
     }
 }
