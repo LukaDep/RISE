8a7242f81c8e52fa3d244d77ddd8118789cf78ec
Seppe Landtsheer <seppe.landtsheer@student.hogent.be>
2025-10-30 09:31:56 +0100

    id's naar string omgevormd

--- src/Rise.Shared/Contact/ContactDto.cs
+++ src/Rise.Shared/Contact/ContactDto.cs
@@ -4,12 +4,12 @@ public static class ContactDto
 {
     public class Index
     {
-        public required int Id { get; set; }
+        public required string Id { get; set; }
         public required string Type { get; set; }
         public required string Name { get; set; }
         public string? Email { get; set; }
         public string? phoneNumber { get; set; }
         public string? ContactPerson { get; set; }
-        public List<int>? Campusses { get; set; }
+        public List<string>? Campusses { get; set; }
     }
 }
