a30ad091267025ad3b14dbe21ceecc6a97a8391a
LukaDep <160613429+LukaDep@users.noreply.github.com>
2025-11-05 09:55:40 +0100

    Add opening and closing time logic to RestoCard component and update resource files

--- src/Rise.Shared/Resto/RestoDto.cs
+++ src/Rise.Shared/Resto/RestoDto.cs
@@ -10,6 +10,9 @@ public static class RestoDto
         public required string BuildingId { get; set; }
         public Dictionary<DayOfWeek, string>? OpeningHours { get; set; }
         public bool IsCurrentlyOpen { get; set; }
+
+        public DateTime? NextOpeningTime { get; set; }
+        public DateTime? NextClosingTime { get; set; }
     public List<string>? KitchenType { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
