e698a18cb8399d3af9c9dcb469005879877f1b52
Seppe Landtsheer <seppe.landtsheer@student.hogent.be>
2025-11-05 09:28:04 +0100

    icoontjes contactcard gefixt

--- src/Rise.Client/Contact/ContactService.cs
+++ src/Rise.Client/Contact/ContactService.cs
@@ -7,7 +7,8 @@ public class ContactService(HttpClient httpClient) : IContactService
     public async Task<Result<ContactResponse.Index>> GetIndexAsync(QueryRequest.SkipTake request, CancellationToken ctx = default)
     {
         //var result = await httpClient.GetFromJsonAsync<Result<ContactResponse.Index>>($"/api/contact?searchterm={request.SearchTerm}&skip={request.Skip}&take={request.Take}", cancellationToken: ctx);
-        var result = await httpClient.GetFromJsonAsync<Result<ContactResponse.Index>>($"/api/contact", cancellationToken: ctx);
+        var result = await httpClient.GetFromJsonAsync<Result<ContactResponse.Index>>($"/api/contact?take=1000", cancellationToken: ctx);
+        Console.Write(result);
         return result!;
     }
 
