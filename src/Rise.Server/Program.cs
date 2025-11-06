22c8e0a53094ba6fe8549d2e4a1e76d7745d74ef
Ninho Decaesteker <ninho.decaesteker@student.hogent.be>
2025-11-06 10:08:48 +0100

    Formatting

--- src/Rise.Server/Program.cs
+++ src/Rise.Server/Program.cs
@@ -100,7 +100,7 @@ try
         {
             var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
             var dbSeeder = scope.ServiceProvider.GetRequiredService<DbSeeder>();
-            
+
             await dbContext.Database.MigrateAsync(); // Creates the database if it doesn't exist and applies all migrations. See Readme.md for more info.
             await dbSeeder.SeedAsync(); // Seeds the database with some test data.
         }
