fafaf8d7c972147af4f58f3fea54712ecbf0cfa5
IIC004 <igorcasteleyn@gmail.com>
2025-11-11 11:56:38 +0100

    linting

--- src/Rise.Client/Contact/ContactService.cs
+++ src/Rise.Client/Contact/ContactService.cs
@@ -7,7 +7,7 @@ public class ContactService(HttpClient httpClient) : IContactService
 {
     public async Task<Result<ContactResponse.Index>> GetIndexAsync(QueryRequest.SkipTake request, CancellationToken ctx = default)
     {
-        var result = await httpClient.GetFromJsonAsync<Result<ContactResponse.Index>>($"/api/contact?searchterm={request.SearchTerm}&skip={request.Skip}&take={request.Take}&filters={JsonSerializer.Serialize(request.Filters)}", cancellationToken: ctx); 
+        var result = await httpClient.GetFromJsonAsync<Result<ContactResponse.Index>>($"/api/contact?searchterm={request.SearchTerm}&skip={request.Skip}&take={request.Take}&filters={JsonSerializer.Serialize(request.Filters)}", cancellationToken: ctx);
         return result!;
     }
 
